ebab2b425d332233a6473e8c1a584063ce9ad8c1
Tux <write@imaginarycode.com>
2016-09-05 22:50:39 -0400

    Added production stuff

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,37 @@ const gulp = require('gulp');
 const less = require('gulp-less');
 const del = require('del');
 const runSequence = require('run-sequence');
+const cleanCss = require('gulp-clean-css');
+const imagemin = require('gulp-imagemin');
+
+const process = require('process');
 
 gulp.task('clean-assets', function() {
     return del('static');
 });
 
 gulp.task('build-assets-css', function() {
-    return gulp.src('assets/css/voxelwind.less')
-        .pipe(less())
-        .pipe(gulp.dest('static'));
+    if (process.env.NODE_ENV === "production") {
+        return gulp.src('assets/css/voxelwind.less')
+            .pipe(less())
+            .pipe(cleanCss())
+            .pipe(gulp.dest('static'));
+    } else {
+        return gulp.src('assets/css/voxelwind.less')
+            .pipe(less())
+            .pipe(gulp.dest('static'));
+    }
 });
 
 gulp.task('build-assets-images', function() {
-    return gulp.src('assets/images/**')
-        .pipe(gulp.dest('static'));
+    if (process.env.NODE_ENV === "production") {
+        return gulp.src('assets/images/**')
+            .pipe(imagemin())
+            .pipe(gulp.dest('static'));
+    } else {
+        return gulp.src('assets/images/**')
+            .pipe(gulp.dest('static'));
+    }
 });
 
 gulp.task('build-assets', function(cb) {
