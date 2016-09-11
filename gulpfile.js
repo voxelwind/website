01add69d7e08007070b36ff03d7567a4d4d6249f
Tux <write@imaginarycode.com>
2016-09-11 03:59:02 -0400

    Removed local copy of normalize.css

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ const del = require('del');
 const runSequence = require('run-sequence');
 const cleanCss = require('gulp-clean-css');
 const imagemin = require('gulp-imagemin');
+const concat = require('gulp-concat');
 
 const process = require('process');
 
@@ -13,13 +14,15 @@ gulp.task('clean-assets', function() {
 
 gulp.task('build-assets-css', function() {
     if (process.env.NODE_ENV === "production") {
-        return gulp.src('assets/css/voxelwind.less')
+        return gulp.src(['node_modules/normalize.css/normalize.css', 'assets/css/voxelwind.less'])
             .pipe(less())
             .pipe(cleanCss())
+            .pipe(concat('voxelwind.css'))
             .pipe(gulp.dest('static'));
     } else {
-        return gulp.src('assets/css/voxelwind.less')
+        return gulp.src(['node_modules/normalize.css/normalize.css', 'assets/css/voxelwind.less'])
             .pipe(less())
+            .pipe(concat('voxelwind.css'))
             .pipe(gulp.dest('static'));
     }
 });
